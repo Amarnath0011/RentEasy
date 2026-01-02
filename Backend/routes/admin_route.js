d2bb0eedba04f3dd3eda1170b91c9e243a654598
AnkitAKS02 <ankitkumarswain2003@gmail.com>
2026-01-02 11:34:44 +0530

    added admin middleware

--- Backend/routes/admin_route.js
+++ Backend/routes/admin_route.js
@@ -6,13 +6,15 @@ import {
   approveListing,
   rejectListing
 } from '../controllers/admin_controller.js'
+import { protectRoute, adminRoute, ownerRoute, restrictTo, userRoute } from '../middleware/auth_middleware.js';
+
 
 const router = express.Router()
 
-router.get('/listings/pending', getAllListingsToApprove)
-router.get('/users', getAllUsers)
-router.get('/owners', getAllOwners)
-router.patch('/listings/:id/approve', approveListing)
-router.patch('/listings/:id/reject', rejectListing)
+router.get('/listings/pending',protectRoute,adminRoute, getAllListingsToApprove)
+router.get('/users',protectRoute,adminRoute, getAllUsers)
+router.get('/owners',protectRoute,adminRoute, getAllOwners)
+router.patch('/listings/:id/approve',protectRoute,adminRoute, approveListing)
+router.patch('/listings/:id/reject',protectRoute,adminRoute, rejectListing)
 
 export default router
